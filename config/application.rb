929ca30fda29c00b1d3517a510ce14a102f9ea02
haryu <hryau6@gmail.com>
2021-11-10 11:19:04 +0900

    add: time zone

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Basic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
     config.generators do |g|
       g.skip_routes true
       g.assets false
