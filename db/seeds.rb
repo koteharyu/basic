583eeec995cd1e12900cf185d311ae35f3841329
haryu <hryau6@gmail.com>
2021-11-10 14:05:01 +0900

    add: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require './seeds/user'
+# require './seeds/posts'
+
+puts 'Start inserting seed "users" ...'
+3.times do
+  user = User.create(
+    email: Faker::Internet.unique.email,
+    username: Faker::Internet.unique.user_name,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+  puts "\"#{user.username}\" has created!"
+end
+
+puts 'Start inserting seed "posts ...'
+User.limit(10).each do |user|
+  post = user.posts.create({ body: Faker::Hacker.say_something_smart, images: [open("#{Rails.root}/db/fixtures/dummy.png")] })
+  puts "post#{post.id} has created!"
+end
