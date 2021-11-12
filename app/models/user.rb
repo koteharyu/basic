e45582058b827300389b44b7a761981488b6405f
haryu <hryau6@gmail.com>
2021-11-12 16:13:27 +0900

    add: function follow and unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,14 @@ class User < ApplicationRecord
   has_many :comments, dependent: :nullify
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
+  has_many :active_relationships, class_name:  'Relationship',
+           foreign_key: 'follower_id',
+           dependent:   :destroy
+  has_many :passive_relationships, class_name:  'Relationship',
+           foreign_key: 'followed_id',
+           dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   scope :randoms, ->(count) { all.sample(count) }
 
@@ -44,6 +52,18 @@ class User < ApplicationRecord
 
   def like?(post)
     like_posts.include?(post)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.destroy(other_user)
+  end
+
+  def following?(other_user)
+    following_ids.include?(other_user)
   end
 
 end
