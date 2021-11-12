82eb9babb9d8f6a4abd23ac7db4dcc0ea61dcd50
haryu <hryau6@gmail.com>
2021-11-12 17:08:08 +0900

    modify: data type to integer of relationships' follower_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_071408) do
+ActiveRecord::Schema.define(version: 2021_11_12_080652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_11_12_071408) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.string "follower_id", null: false
+    t.integer "follower_id", null: false
     t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
