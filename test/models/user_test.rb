2184e5a3f00f5b9a4d0f61b7673b9fa21e17de57
haryu <hryau6@gmail.com>
2021-11-16 17:10:50 +0900

    add: notification flags to user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,14 +2,17 @@
 #
 # Table name: users
 #
-#  id               :bigint           not null, primary key
-#  avatar           :json
-#  crypted_password :string
-#  email            :string           not null
-#  salt             :string
-#  username         :string           not null
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
+#  id                      :bigint           not null, primary key
+#  avatar                  :json
+#  crypted_password        :string
+#  email                   :string           not null
+#  notification_on_comment :boolean          default(TRUE)
+#  notification_on_follow  :boolean          default(TRUE)
+#  notification_on_like    :boolean          default(TRUE)
+#  salt                    :string
+#  username                :string           not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
 #
 # Indexes
 #
