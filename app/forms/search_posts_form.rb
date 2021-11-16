5d6faa59fae4b57f486e1bc2ac0050220dc97aeb
haryu <hryau6@gmail.com>
2021-11-16 12:48:17 +0900

    add: search with some words

--- app/forms/search_posts_form.rb
+++ app/forms/search_posts_form.rb
@@ -8,9 +8,15 @@ class SearchPostsForm
 
   def search
     scope = Post.all
-    scope = scope.body_contain(body) if body.present?
+    scope = splited_bodies.map { |splited_body| scope.body_contain(splited_body) }.inject { |result, scp| result.or(scp) } if body.present?
     scope = scope.comment_body(comment_body) if comment_body.present?
     scope = scope.username_contain(username) if username.present?
     scope
+  end
+
+  private
+
+  def splited_bodies
+    body.strip.split(/[[:blank:]]+/)
   end
 end
