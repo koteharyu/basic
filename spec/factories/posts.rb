f38e2e8bc87580e241c4536217a34c32208ec3ba
haryu <hryau6@gmail.com>
2021-11-17 11:32:52 +0900

    add: model spec of user and post

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factoy :post do
+  factory :post do
     body { Faker::Hacker.say_something_smart }
     images { [File.open("#{Rails.root}/spec/fixtures/TacoRice.jpeg")] }
     user
